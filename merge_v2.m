adec4e35c39e15d93f0cec4099823d2430c8cd51
ajburjek <ajburjek@crimson.ua.com>
2019-09-10 13:51:35 -0500

    Cleaned Up
    Mean anomaly and Range is calculated for a time range

--- merge_v2.m
+++ merge_v2.m
@@ -1,34 +1,31 @@
 %Two Body Problem
-e = 0.01;
-a = 6871000; %m, 500 km altitude
-i = 90; %deg
-w = 0; %deg
-RAAN = 0; %deg
-f = 0; %deg
-M = zeros(1,100); 
+
+%Orbital Elements
+e = 0.01; %eccentricity
+a = 6871000; %semimajor axis, meters, 500 km altitude
+i = 90; %inclination deg
+w = 0; %argument of perigee, deg
+RAAN = 0; %%ascending node, deg
+f = 0; %True anomaly, deg
+M = zeros(1,100); %Mean anomaly, deg
 range = zeros(1,100); 
 M(1) = 0; 
-elements = [a, e, i, RAAN, w, M];
 
 %Ground station position
 x2 = 228832.435;
 y2 =  -5336696.538;
 z2 = 3473801.891;
 
-t=1;
-
+%Defining variables
+t0 = 0;
 mu = 3.9860044e+14; %m^3/s^2
-
-a    = elements(1);%semimajor axis, meters
-e    = elements(2);%eccentricity
-i    = elements(3);%inclination, degrees
 Sini = sind(i); Cosi = cosd(i);
-raan = elements(4);%ascending node, degrees
 Sinraan = sind(raan); Cosraan = cosd(raan);
-w    = elements(5);%aop, degrees
 Sinw = sind(w); Cosw = cosd(w);
-M    = elements(6);%mean anomaly, degrees
+n = sqrt(mu/(a^3));
+tp = t0 - (M(1)/n);
 
+t=1;
 %ekf initials 
 %{
 pstate = zeros(1,100);
@@ -36,12 +33,11 @@ pcov = zeros(1,100);
 cstate = zeros(1,100);
 ccov = zeros(1,100);
 %}
-corrected = zeros(1,100);
-true = zeros(1,100);
+%corrected = zeros(1,100);
+%true = zeros(1,100);
 
-t0 = 0;
-n = sqrt(mu/(a^3));
-tp = t0 - (M(1)/n);
+
+
 
 while t<100
     Sm = sind(M); Cm = cosd(M);
@@ -79,13 +75,13 @@ while t<100
     range(t) = sqrt(((x1-x2)^2)+((y1-y2)^2)+((z1-z2)^2));
     %rang = append(rang, range); %FIXME append list
     
-    %trackingEKF on Automated Driving System Toolbox
-    obj = extendedKalmanFilter(@vdpStateFcn,@vdpMeasurementFcn,initialState);
-    obj.ProcessNoise = 1.0;  
-    obj.MeasurementNoise = 0.01; % 0.1^2
+    %%trackingEKF on Automated Driving System Toolbox
+    %obj = extendedKalmanFilter(@vdpStateFcn,@vdpMeasurementFcn,initialState);
+    %obj.ProcessNoise = 1.0;  
+    %obj.MeasurementNoise = 0.01; % 0.1^2
     
-    [PredictedState,PredictedStateCovariance] = predict(obj);
-    [CorrectedState,CorrectedStateCovariance] = correct(obj,range(t));
+    %[PredictedState,PredictedStateCovariance] = predict(obj);
+    %[CorrectedState,CorrectedStateCovariance] = correct(obj,range(t));
 
     %{
     pstate(t) = PredictedState;
@@ -94,12 +90,12 @@ while t<100
     ccov(t) = CorrectedStateCovariance;
     %}
     
-    corrected(t) = sqrt(((x1-x2)^2)+((y1-y2)^2)+((z1-z2)^2)); %FIXME
+    %corrected(t) = sqrt(((x1-x2)^2)+((y1-y2)^2)+((z1-z2)^2)); %FIXME
     
-    true(t) = range(t) - corrected(t);
+    %true(t) = range(t) - corrected(t);
 
-    %Time step
     
+    %%Time step
     M(t+1) = n*(t-tp);
     t = t + 1;
 end
