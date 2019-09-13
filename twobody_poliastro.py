6810dbdeceb791386835006548b5b357e0362918
Christopher R. Simpson <crsimpson@crimson.ua.edu>
2019-09-12 20:56:57 -0500

    Change plot/del files Updated plot to 3d, deleted old code, and started working on merging code

--- twobody_poliastro.py
+++ twobody_poliastro.py
@@ -18,13 +18,10 @@ raan = 292.1260 * u.deg
 aop  = 28.6368 * u.deg
 nu   = 331.5244 * u.deg
 ss = Orbit.from_classical(Earth, a, ecc, inc, raan, aop, nu)
-ss.plot()
+ss.plot(use_3d=True)
 
 X = np.empty([6,1])
 t = np.linspace(0,86400,num=86401)
 for i in range(0,t.size):
     ss_update = ss.propagate(t[i]*u.second)
-#    if i==0:
-#        X = np.insert(X, 0, [[ss_update.r[0].value], [ss_update.r[1].value], [ss_update.r[2].value], [ss_update.v[0].value], [ss_update.v[1].value], [ss_update.v[2].value]], axis=1)
-#    else:
     X = np.append(X, [[ss_update.r[0].value], [ss_update.r[1].value], [ss_update.r[2].value], [ss_update.v[0].value], [ss_update.v[1].value], [ss_update.v[2].value]], axis=1)
