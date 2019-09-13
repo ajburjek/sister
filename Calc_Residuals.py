268a6e0641bf1f156ecce2c65787c6ec74190fb9
Christopher R. Simpson <crsimpson@crimson.ua.edu>
2019-09-13 15:56:38 -0500

    Calculate residual between computed range and "real" range

--- Calc_Residuals.py
+++ Calc_Residuals.py
@@ -5,56 +5,20 @@ Created on Tue Sep 10 14:06:26 2019
 
 @author: willjpatton
 """
-
 import numpy as np
+from astropy import constants as const
 
-
-'Ground Station Location
-Xg = 
-Yg = 
-Zg = 
-
-interval = 1 #sec
-
-def Range ():
-    #x y and z values from range and ground station
-    range = np.sqrt(((X-Xg)**2)+((Y-Yg)**2)+((Z-Zg)**2))
-    return range
-
-#elevation
-
-def Time_Recieved (range,interval):
-        # time recieved from range calculation
-        c = 299792458 #m/s
-        time_received = interval + range/c;
-        return time_recieved
+def Time_Received (rho,interval):
+        # time received from range calculation
+        time_received = interval + rho/const.c;
+        return time_received
 
 def Alter_Time (time_received):
     #change time recieved by some value for testing
-    time_altered = time_recieved*1.1
+    time_altered = time_received*np.random.random_sample()
     return time_altered
 
 def Residuals (time_received,time_altered):
     #return time residuals by taking differences of time recieved and altered
     residual = np.abs(time_received-time_altered)
     return residual
-'''
-need rt and zt to find elevation
-
-
-if -90 < El < 90 deg
-elevation = np.asin(z/r)
-else
-end
-'''
-
-range = Range()
-
-time_recieved = Time_Received(range,interval)
-
-time_altered = Alter_Time(time_received)
-
-residual = Residuals(time_received,time_altered)
-
-
-
--- twobody_poliastro.py
+++ twobody_poliastro.py
@@ -9,6 +9,7 @@ from poliastro.twobody import Orbit
 from poliastro.twobody.propagation import mean_motion
 from astropy.coordinates import EarthLocation, SkyCoord, AltAz, GCRS, ICRS
 from astropy.time import Time, TimeDelta
+from Calc_Residuals import Time_Received, Alter_Time, Residuals
 
 
 """Python 3.7
@@ -23,7 +24,7 @@ t_ob = Time(Time.now(),scale='tai')
 delta_t = TimeDelta(np.linspace(0,86400,num=86401)*u.second,scale='tai')
 t_del_nounit = np.linspace(0,86400,num=86401)*u.second
 
-
+t_interval = 10*u.second
 #topocentric frame at Hardaway Hall
 frame_now_hh = AltAz(obstime=t_ob+delta_t,location=loc_hh)
 #ISS
@@ -39,3 +40,6 @@ X = mean_motion(Earth.k, ss.r, ss.v, t_del_nounit)
 ss_SkyCoord = SkyCoord(X[0][:,0], X[0][:,1], X[0][:,2], frame = 'gcrs', representation_type='cartesian')
 ssaltaz_hh = ss_SkyCoord.transform_to(frame_now_hh)
 
+time_rec = Time_Received(ssaltaz_hh.distance,t_interval)
+time_alt = Alter_Time(time_rec)
+residual = Residuals(time_rec,time_alt)
