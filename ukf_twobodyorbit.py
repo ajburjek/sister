9edcd59b430a635f5f5f22476af8ad1f0ab1911a
crsimpson <crsimpson@crimson.ua.edu>
2019-09-20 00:27:33 -0500

    Removed unecessary lines except for STM calculation

--- ukf_twobodyorbit.py
+++ ukf_twobodyorbit.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Two-Body Orbit"""
-import astropy.units as u
-import numpy as np
-#import matplotlib.pyplot as plt
-from filterpy.kalman import UnscentedKalmanFilter, unscented_transform
+from twobody_poliastro import hh_obs
 from poliastro.bodies import Earth
-from poliastro.twobody import Orbit
-#from poliastro.twobody.propagation import mean_motion
-#from astropy.coordinates import EarthLocation, SkyCoord, AltAz, GCRS, ICRS
-#from astropy.time import Time, TimeDelta
-#from Calc_Residuals import Time_Received, Alter_Time, Residuals
+from filterpy.kalman import UnscentedKalmanFilter, unscented_transform
+
 
 
 """Python 3.7
@@ -46,3 +40,6 @@ def stm(attractbody, state):
     lam_jk = (3*attractbody.k*state.r[1]*state.r[2])/(r_mag5)
     lam = np.array([[lam_ii+update_pos, lam_ij, lam_ik],[lam_ij, lam_jj+update_pos, lam_jk], [lam_ik, lam_jk, lam_kk+update_pos]])
     return lam
+
+Y_comp, X_comp = hh_obs()
+X_STM = stm(Earth, X_comp)
