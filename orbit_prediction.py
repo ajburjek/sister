e216d55d7ce5529aab03db05218d434d702b591b
William Patton <wjpatton1@crimson.ua.edu>
2019-10-29 22:26:08 -0500

    Conda Bug
    Trying to fix a bug where mean motion won't work when using conda. Current issue is a 'Quantity' object with a scalar value that is not iterable.

--- orbit_prediction.py
+++ orbit_prediction.py
@@ -8,7 +8,6 @@ from poliastro.core.angles import D_to_nu
 #from astropy.coordinates import EarthLocation, SkyCoord, AltAz, GCRS, ICRS
 from poliastro.bodies import Earth
 from poliastro.twobody import Orbit
-from poliastro.twobody.propagation import mean_motion
 #custom
 import readtle
 from downloadfileurl import download_file
@@ -25,6 +24,16 @@ from time_helper import daysToMonDayHrMinSec, jday
 rooturl = 'http://www.celestrak.com/NORAD/elements/supplemental/'
 iss_tles = download_file(rooturl+'iss.txt')
 coe, yd = readtle.readtle(iss_tles)
+
+
+def mean_motion(k, r, v, tofs, **kwargs):
+    
+    results = [mean_motion(k, r, v, tof) for tof in tofs]
+    return (
+        [result[0] for result in results] * u.km,
+        [result[1] for result in results] * u.km / u.s,
+    )
+    
 
 julianday = np.empty([0])
 i=0
@@ -45,7 +54,13 @@ Xf_tle_n = Orbit.from_classical(Earth,
                                 nu*u.deg,
                                 epoch = epoch_tles[yd.shape[1]-1])
 
-Xf_n = mean_motion(Earth.k, Xf_tle_n.r, Xf_tle_n.v, np.linspace(0,0,num=1)*u.s)
+k = Earth.k
+r = Xf_tle_n.r
+v = Xf_tle_n.v
+tofs = np.linspace(0.0,0.0,num=1)*u.s
+
+Xf_n = mean_motion(k,r,v,tofs)
+
 # fill orbit
 i=0
 X_res = np.empty((6,1))
@@ -59,7 +74,8 @@ for i in range(yd.shape[1]-1):
                                     coe[4,i]*u.deg, 
                                     nu*u.deg,
                                     epoch = epoch_tles[i])
-    tof = np.linspace((epoch_tles[yd.shape[1]-1]-epoch_tles[i]).sec,(epoch_tles[yd.shape[1]-1]-epoch_tles[i]).sec, num=1)*u.s
+    tof = np.linspace((epoch_tles[yd.shape[1]-1]-epoch_tles[i]).sec,
+                      (epoch_tles[yd.shape[1]-1]-epoch_tles[i]).sec, num=1)*u.s
     Xf_tle_i = mean_motion(Earth.k, X0_tle_i.r, X0_tle_i.v, tof)
     temp_r = np.array([Xf_tle_i[0][0,:]-Xf_n[0][0,:]]).reshape(3,1)
     temp_v = np.array([Xf_tle_i[1][0,:]-Xf_n[1][0,:]]).reshape(3,1)
